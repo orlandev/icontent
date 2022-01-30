e1a205f3a007487fafe649f608d581c947eba73e
orlandev <dfashion.corp@gmail.com>
2022-01-30 10:13:29 -0600

    Add parameter for the context that we need to load a resource image

--- icontent/src/main/java/com/orlandev/icontent/models/IContentType.kt
+++ icontent/src/main/java/com/orlandev/icontent/models/IContentType.kt
@@ -1,10 +1,12 @@
 package com.orlandev.icontent.models
 
+import android.content.Context
 import androidx.annotation.DrawableRes
 
 sealed interface IContentType {
+    //TODO ( CHANGE TEXT TO A CLASS AND ADD TO THIS THE STYLE FOR THE TEXT )
     object Text : IContentType
-    class Image(@DrawableRes val noImageFound: Int) : IContentType
+    class Image(@DrawableRes val noImageFound: Int, val context: Context) : IContentType
     object Video : IContentType
     object Pano : IContentType
 }
