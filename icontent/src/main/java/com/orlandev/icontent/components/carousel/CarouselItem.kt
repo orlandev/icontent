e920d415e76ef6ef36563ab1f43e83cf4c344637
Orlando Novas Rodriguez <onovas05@gmail.com>
2022-03-31 10:25:24 -0400

    🚀  Add padding in title and subtitle texts of CarouselItem.kt

--- icontent/src/main/java/com/orlandev/icontent/components/carousel/CarouselItem.kt
+++ icontent/src/main/java/com/orlandev/icontent/components/carousel/CarouselItem.kt
@@ -3,6 +3,7 @@ package com.orlandev.icontent.components.carousel
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -10,10 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.orlandev.icontent.components.IImageBlur
 import com.orlandev.icontent.models.ContentModel
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,9 +38,13 @@ fun CarouselItem(
                 contentModel = imageContent,
                 contentType = imageContent.typeI
             )
-            Column(modifier = Modifier.align(Alignment.BottomStart)) {
-                Text(text = title)
-                Text(text = subtitle)
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(8.dp)
+            ) {
+                Text(text = title, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                Text(text = subtitle, maxLines = 1, overflow = TextOverflow.Ellipsis)
             }
         }
     }
