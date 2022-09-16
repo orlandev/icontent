ada83eb3baedca9a2de1a8176128515731bb4575
Orlando Novas Rodriguez <38017009+orlandev@users.noreply.github.com>
2022-09-16 15:00:38 -0400

    🚀  Upgrade dependencies and change IImageBlur.kt implementation

--- icontent/src/main/java/com/orlandev/icontent/components/IImageBlur.kt
+++ icontent/src/main/java/com/orlandev/icontent/components/IImageBlur.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import coil.annotation.ExperimentalCoilApi
-import com.ondev.imageblurkt_lib.AsyncImageBlur
+import com.ondev.imageblurkt_lib.AsyncBlurImage
+import com.ondev.imageblurkt_lib.IBlurModel
 import com.orlandev.icontent.R
 import com.orlandev.icontent.models.ContentModel
 import com.orlandev.icontent.models.IContentType
@@ -21,28 +23,41 @@ fun IImageBlur(
     modifier: Modifier,
     contentModel: ContentModel,
     contentType: IContentType,
+    contentScale: ContentScale = ContentScale.Crop,
     addGradient: Boolean = false,
     gradientColor: Color = MaterialTheme.colorScheme.background,
-    contentScale: ContentScale = ContentScale.Crop
+    orientation: GradientEffectOrientation = GradientEffectOrientation.Vertically,
+    align: GradientAlignment = GradientAlignment.End,
+    alphaValue: Float = 0.9f
 ) {
-    val currentResources = LocalContext.current.resources
-    val imgRef = contentModel.field.split(FIELD_IMAGE_BLUR_DELIMITIER)
-    if (imgRef.size == 2 && contentType is IContentType.Image) {
+    val data = remember {
+        derivedStateOf {
+            val imgRef = contentModel.field.split(FIELD_IMAGE_BLUR_DELIMITIER)
+            if (imgRef.size == 2 && contentType is IContentType.Image) {
+                IBlurModel(imageUrl = imgRef[0], blurHash = imgRef[1])
+            } else {
+                null
+            }
+        }
+    }
+
+    data.value?.let { currentData ->
         Box(modifier = Modifier.wrapContentSize()) {
-            AsyncImageBlur(
+            AsyncBlurImage(
                 modifier = modifier,
-                imageUrl = imgRef[0], //Image URL
-                blurHash = imgRef[1], //Blurhash
-                resources = currentResources,
+                data = currentData,
                 notImageFoundRes = contentModel.noImageFound ?: R.drawable.no_image,
                 contentDescription = null,
                 contentScale = contentScale
             )
             if (addGradient) {
-                IGradientEffect(backgroundColor = gradientColor)
+                IGradientEffect(
+                    backgroundColor = gradientColor,
+                    orientation = orientation,
+                    align = align,
+                    alphaValue = alphaValue
+                )
             }
         }
-    } else {
-        throw Exception("Invalid content type")
     }
 }
