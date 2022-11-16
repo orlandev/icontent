e8a4ea63e9fb68d66b71c94f25648732550fd5bc
Orlando N. Rodriguez <onovas05@gmail.com>
2022-11-16 05:34:31 -0500

    Upgrade dependencies and some implementations

--- icontent/src/main/java/com/orlandev/icontent/components/IExpandText.kt
+++ icontent/src/main/java/com/orlandev/icontent/components/IExpandText.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -15,7 +14,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.orlandev.icontent.utils.smartTruncate
-
 
 
 /***
@@ -42,41 +40,37 @@ fun IExtendText(
     var expanded by remember { mutableStateOf(false) }
 
     Column(modifier = modifier, horizontalAlignment = Alignment.End) {
-        AnimatedContent(
-            modifier = Modifier.fillMaxWidth(),
+        AnimatedContent(modifier = Modifier.fillMaxWidth(),
             targetState = expanded,
             transitionSpec = {
                 // Compare the incoming number with the previous number.
                 if (expanded) {
                     // If the target number is larger, it slides up and fades in
                     // while the initial (smaller) number slides up and fades out.
-                    fadeIn() with
-                            fadeOut()
+                    fadeIn() with fadeOut()
                 } else {
                     // If the target number is smaller, it slides down and fades in
                     // while the initial number slides down and fades out.
-                    fadeIn() with
-                            fadeOut()
+                    fadeIn() with fadeOut()
                 }.using(
                     // Disable clipping since the faded slide-in/out should
                     // be displayed out of bounds.
                     SizeTransform(clip = false)
                 )
-            }
-        ) { targetExpanded ->
-            Text(
+            }) { targetExpanded ->
+            IText(
                 modifier = Modifier.fillMaxWidth(),
                 text = if (targetExpanded) text else text.smartTruncate(
                     max = maxTextTruncate
                 ),
-                color = MaterialTheme.colorScheme.onBackground,
                 style = style,
             )
         }
         IconButton(onClick = { expanded = !expanded }) {
             Icon(
                 if (!expanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowUp,
-                contentDescription = "", tint = MaterialTheme.colorScheme.onBackground
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.onBackground
             )
         }
     }
